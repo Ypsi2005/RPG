207ffe10aa384eb0416e1cfe526385bbd672d321
KJupp <renehenrich@hotmail.de>
2018-11-16 22:32:37 +0100

    Character Script 1.0

--- Rpg/Assets/Script/Character.cs
+++ Rpg/Assets/Script/Character.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public abstract class Character : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public int Hp { get; set; }
+    public int Mana { get; set; }
+    public int Range { get; set; }
+    public int Lvl { get; set; }
+
+    public int AtK { get; set; }
+    public int Def{ get; set; }
+    public int Mag { get; set; }
+    public int MDF { get; set; }
+    public int Agi { get; set; }
+    public int Luk { get; set; }
+    public int Acc { get; set; }
+    public int Cha { get; set; }
+    public int Fok { get; set; }
+    public int Wis { get; set; }
+    public int Dex { get; set; }
+
+    public int Fire { get; set; }
+    public int Ice { get; set; }
+    public int Poison { get; set; }
+    public int Lighting { get; set; }
+    public int Unholy { get; set; }
+    public int Holy { get; set; }
+
+
+
+    public virtual void Move(int agi)
+    {
+
+    }
+
+    public virtual void DmgDone()
+    {
+
+    }
 }
